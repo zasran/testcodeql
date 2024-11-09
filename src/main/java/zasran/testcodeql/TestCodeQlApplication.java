71428ec7c009fda18a47a2afebc08896483e0dca
Erik Steffl <erik.steffl@qlik.com>
2024-11-08 17:15:57 -0800

    initiail commit

---
+++ src/main/java/zasran/testcodeql/TestCodeQlApplication.java
@@ -0,0 +1,13 @@
+package zasran.testcodeql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCodeQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zasran/testcodeql/TestCodeQlApplicationTests.java
@@ -0,0 +1,13 @@
+package zasran.testcodeql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
